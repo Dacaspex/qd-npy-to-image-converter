516452a37650419d5f0739099db1ecff5a9714e1
Casper Smits <caspersmits11@outlook.com>
2018-09-10 11:59:45 +0200

    Added separation of validation and training data

--- import.py
+++ import.py
@@ -9,20 +9,36 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 console_output_interval = 10
 start_time = 0
 image_limit = 100
+training_split = 0.95
+
+# Get command line arguments, if present
+if len(sys.argv) == 2:
+    image_limit = sys.argv[1]
+elif len(sys.argv) == 3:
+    image_limit = sys.argv[1]
+    training_split = sys.argv[2]
 
 # Start image generation
 start_time = current_milli_time()
 for file in os.listdir('./data'):
+    # Process image name
     image_name = file.split('.')[0]
+    if 'full%2Fnumpy_bitmap%2F' in image_name:
+        image_name = image_name[22:].replace(' ', '_')
     print('Processing image: ', image_name)
 
-    # Create output directory
-    if not os.path.exists('output/' + image_name):
-        os.makedirs('output/' + image_name)
+    # Create output directories
+    if not os.path.exists('output/training/' + image_name):
+        os.makedirs('output/training/' + image_name)
+    if not os.path.exists('output/validation/' + image_name):
+        os.makedirs('output/validation/' + image_name)
 
     # Load data from file
     data = np.load('data/' + file)
     total_size = data.size
+
+    if total_size < image_limit or image_limit == -1:
+        image_limit = total_size
 
     # Loop over the array
     for i in range(image_limit):
@@ -35,7 +51,16 @@ for file in os.listdir('./data'):
         # Create image
         image_data = data[i].reshape(28, 28)
         image = Image.fromarray(image_data)
-        image_output_path = 'output/' + image_name + '/' + image_name + '-' + str(i) + '.png'
+
+        # Determine training or validation folder
+        folder = ''
+        if i >= image_limit * training_split:
+            folder = 'validation/'
+        else:
+            folder = 'training/'
+
+        # Save image
+        image_output_path = 'output/' + folder + image_name + '/' + image_name + '-' + str(i) + '.png'
         image.save(image_output_path)
 
 total_elapsed_time = current_milli_time() - start_time
